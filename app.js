f2b5525d985ab569b86da150b9abb8a2636b034e
Jia Li <tomyleee@163.com>
2018-05-13 12:51:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var settings = require('./settings');
+var flash = require('connect-flash');
 
 var app = express();
 
 //初始化View
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(flash());
 
 //app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
@@ -19,6 +23,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+app.use(session({
+  // secret: settings.cookieSecret,
+  // key: settings.db,//cookie name
+  // cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },//30 days
+  // store: new MongoStore({
+  //   db: settings.db,
+  //   host: settings.host,
+  //   port: settings.port
+  // })
+  secret: settings.cookieSecret,
+  key: settings.db,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    url: 'mongodb://localhost/' + settings.db
+  })
+}));
 
 //路由
 routes(app);
@@ -34,7 +62,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   //渲染错误页
-  res.status(err.status || 500);
+  res.status(err.status);
   res.render('error');
 });
 
