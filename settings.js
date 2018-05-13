48b427fb530b9008406f77f4b61ceb814d20a3b2
Jia Li <1335275309@qq.com>
2018-05-13 14:05:57 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+   //cookieSecret 用于 Cookie 加密与数据库无关
+    cookieSecret: 'myorder', 
+    //db是数据库名称
+    db: 'pipiorder',
+    //地址和端口
+    host: 'localhost',
+    port: 27017
+}; 
