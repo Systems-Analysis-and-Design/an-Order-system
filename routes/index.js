7c5f03b10972a9e2bed2b377fca10fa0acf35c7d
Dennis <937889621@qq.com>
2018-05-13 22:43:40 +0800

    small changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var path = require('path');
-var crypto = require('crypto'),
-  User = require('../models/user.js');
-var mime = require('../models/mime');
+var url = require('url');
+var crypto = require('crypto');
+var User = require('../models/user');
 
 module.exports = function(app) {
   /* GET home page. */
@@ -10,66 +10,93 @@ module.exports = function(app) {
     res.render('home', { title: '皮皮怪点餐' });
   });
 
-
-  //app.post('/regist', checkNotLogin);
-  app.post('/regist', function (req, res) {
-    var name = req.body.username,
-      password = req.body.password;
-
-    //生成密码的 md5 值
-    var md5 = crypto.createHash('md5'),
-      password = md5.update(req.body.password).digest('hex');
-    var newUser = new User({
-      name: req.body.username,
-      password: password,
-      phone: req.body.phone,
-      email: req.body.email
-    });
-    //检查用户名是否已经存在 
-    User.get(newUser.name, function (err, user) {
-      if (err) {
-        // req.flash('error', err);
-        // return res.redirect('/');
-        return res.json(err);
-      }
-      if (user) {
-        // req.flash('error', '用户已存在!');
-        // return res.redirect('/');//返回注册页
-        return res.json("exited");
-      }
-      //如果不存在则新增用户
-      newUser.save(function (err, user) {
-        if (err) {
-          // req.flash('error', err);
-          // return res.redirect('/');//注册失败返回主册页
-          return res.json(err);
-        }
-        req.session.user = user;//用户信息存入 session
-        // req.flash('success', '注册成功!');
-        return res.json("success");
-       // res.redirect('/');
+  app.post('/', function (req, res) {
+    //表单类型
+    var type = url.parse(req.url).query;
+    if (type == 'regist') {
+      //生成密码的 md5 值
+      var md5 = crypto.createHash('md5');
+      var password = md5.update(req.body.password).digest('hex');
+      var newUser = new User({
+        name: req.body.username,
+        password: password,
+        phone: req.body.phone,
+        email: req.body.email
       });
-    });
-    
-  });
+      console.log(newUser.name);
+      //检查用户名是否已经存在 
+      User.get(newUser.name, function (err, user) {
+        if (err) {
+            return res.json(err);
+        }
+        if (user) {
+            return res.json("exited");
+        }
+        newUser.save(function (err, user) {
+            if (err) {
+                return res.json(err);
+            }
+            req.session.user = user;
+            return res.json("success");
+        });
+      });
+    } else if(type == 'login') {
+      //登录表单处理
+    }
+  }
 
-  // function checkLogin(req, res, next) {
-  //   if (!req.session.user) {
-  //     res.json(err);
-  //     res.redirect('/');
-  //   }
-  //   next();
-  // }
-
-  // function checkNotLogin(req, res, next) {
-  //   if (req.session.user) {
-  //     res.json(err);
-  //     res.redirect('/');
-  //   }
-  //   next();
-  // }
   app.use(function (req, res) {    //获取css,js,img
     return res.sendFile(__dirname + '../public' + req.url);
   });
+}
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var crypto = require('crypto');
+var User = require('../models/user');
 
+module.exports = function(app) {
+  /* GET home page. */
+  app.get('/', function(req, res, next) {
+    res.render('home', { title: '皮皮怪点餐' });
+  });
+
+  app.post('/', function (req, res) {
+    //表单类型
+    var type = url.parse(req.url).query;
+    if (type == 'regist') {
+      //生成密码的 md5 值
+      var md5 = crypto.createHash('md5');
+      var password = md5.update(req.body.password).digest('hex');
+      var newUser = new User({
+        name: req.body.username,
+        password: password,
+        phone: req.body.phone,
+        email: req.body.email
+      });
+      console.log(newUser.name);
+      //检查用户名是否已经存在 
+      User.get(newUser.name, function (err, user) {
+        if (err) {
+            return res.json(err);
+        }
+        if (user) {
+            return res.json("exited");
+        }
+        newUser.save(function (err, user) {
+            if (err) {
+                return res.json(err);
+            }
+            req.session.user = user;
+            return res.json("success");
+        });
+      });
+    } else if(type == 'login') {
+      //登录表单处理
+    }
+  }
+
+  app.use(function (req, res) {    //获取css,js,img
+    return res.sendFile(__dirname + '../public' + req.url);
+  });
 }
