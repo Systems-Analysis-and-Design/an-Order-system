d99d2600352228d17bec95321d6a46adc1c3253c
Dennis <937889621@qq.com>
2018-05-25 23:13:53 +0800

    v0.5

---
+++ public/javascripts/checkForm.js
@@ -0,0 +1,52 @@
+//待补全，仅支持注册和登录表单的检查
+function checkValidity() {    //检查合法性
+  if($(".form-result").text()) {    //清空#result里的内容
+    $(".form-result").text("");
+  }
+
+  var value = $(this).val();
+  if (value == "") return;
+  var objId = $(this).attr("id"), flag;
+  switch(objId) {
+    case "username":
+    case "upUsername":
+      flag = /^[a-zA-Z]\w{5,17}$/.test(value);
+      break;
+    case "password":
+    case "upPassword":
+      flag = /[a-zA-Z_0-9-]{6,12}/.test(value);
+      break;
+    case "repassword":
+      flag = (value == $("#upPassword").val()) && /[a-zA-Z_0-9-]{6,12}/.test(value);
+      break;
+    case "phone":
+      flag = /^[1-9]\d{10}$/.test(value);
+      break;
+    case "email":
+      flag = /^[a-zA-Z_\-0-9]+@(([a-zA-Z_\-0-9])+\.)+[a-zA-Z0-9]{2,4}$/.test(value);
+      break;
+    //待修改
+    case "storeName":
+      flag = true;
+      break;
+    case "storeAddress":
+      flag = true;
+      break;
+  }
+  showValidity(flag, objId);
+}
+
+function showValidity(flags, id) {    //显示是否合法的图标
+  var icon = $("#" + id).next();
+  icon.attr("src", "http://" + window.location.host + (flags ? "/images/correct.png" : "/images/error.png"));
+  icon.css("opacity", 1);
+}
+
+function isAllCorrect(form) {    //全部合法才能提交
+  var corrcetPath = "http://" +window.location.host + "/images/correct.png";
+  var flag = true;
+  $("[name='" + form + "'] .form-body img").each(function() {
+    if($(this).attr("src") != corrcetPath) flag = false;
+  });
+  return flag;
+}
