b766d0c4ddeec4af5e228deee184fdabb406d85d
huixuli <1967250954@qq.com>
2018-05-26 14:11:02 +0800

    Add files via upload

---
+++ models/employee.js
@@ -0,0 +1,65 @@
+var mongodb = require('./db');
+
+function Employee(employee) {
+	this.owner = employee.owner;
+    this.account = employee.password;
+    this.password = employee.password
+    this.name = employee.name;
+    this.age = employee.age;
+    this.phone = employee.phone;
+    this.position = employee.position;
+};
+
+Employee.prototype.save = function (callback) {
+    //要存入数据库的用户文档
+    var employee = {
+    	account: this.account,
+        password: this.password,
+        name: this.name,
+        age: this.age
+        phone:this.phone,
+        position: this.position
+    };
+    //打开数据库
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);//错误，返回 err 信息
+        }
+        //读取  employees 集合
+        db.collection(this.owner + 'employees', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);//错误，返回 err 信息
+            }
+            //将员工数据插入 employees 集合
+            collection.insert(employee, {safe: true}, function (err, employee) {
+                mongodb.close();
+                //成功！err 为 null，并返回存储后的用户文档
+                return err ? callback(err) : callback(null, employee.ops[0]);
+            });
+        });
+    });
+};
+
+//读取员工信息
+Employee.get = function (account, callback) {
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);//错误，返回 err 信息
+        }
+        //读取 employees 集合
+        db.collection(this.owner + 'employees', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);//错误，返回 err 信息
+            }
+            //查找账户（值为 account 一个文档
+            collection.findOne({account: account}, function (err, employee) {
+                mongodb.close();
+                return err ? callback(err) : callback(null, employee);
+            });
+        });
+    });
+};
+
+module.exports = Employee;
---
+++ models/ingredient.js
@@ -0,0 +1,62 @@
+var mongodb = require('./db');
+
+function Ingredient(ingredient) {
+    this.owner = ingredient.owner;
+    this.name = ingredient.name;
+    this.price = ingredient.price;
+    this.cost = ingredient.cost;
+    this.stock = ingredient.stock; 
+};
+
+//存储用户信息
+User.prototype.save = function (callback) {
+    //要存入数据库的用户文档
+    var ingredient = {
+        name: this.name,
+        price: this.price,
+        cost:this.cost,
+        stock: this.stock
+    };
+    //打开数据库
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);//错误，返回 err 信息
+        }
+        //读取 users 集合
+        db.collection(this.owner + 'ingredients', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);//错误，返回 err 信息
+            }
+            //将用户数据插入 users 集合
+            collection.insert(ingredient, {safe: true}, function (err, ingredient) {
+                mongodb.close();
+                //成功！err 为 null，并返回存储后的用户文档
+                return err ? callback(err) : callback(null, ingredient.ops[0]);
+            });
+        });
+    });
+};
+
+//读取用户信息
+User.get = function (name, callback) {
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);//错误，返回 err 信息
+        }
+        //读取 users 集合
+        db.collection(this.owner + 'ingredients', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);//错误，返回 err 信息
+            }
+            //查找用户名（name键）值为 name 一个文档
+            collection.findOne({name: name}, function (err, user) {
+                mongodb.close();
+                return err ? callback(err) : callback(null, ingredient);
+            });
+        });
+    });
+};
+
+module.exports = Ingredient;
