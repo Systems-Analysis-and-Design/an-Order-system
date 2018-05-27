d234270d95a63b65dd5105995eb63f8846724c8b
huixuli <1967250954@qq.com>
2018-05-27 14:36:39 +0800

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -1,31 +1,33 @@
+
 var mongodb = require('./db');
 
 function Employee(employee) {
 	this.owner = employee.owner;
-    this.account = employee.password;
-    this.password = employee.password
+    this.usernmae = employee.username;
+    this.password = employee.password;
     this.name = employee.name;
     this.age = employee.age;
     this.phone = employee.phone;
-    this.position = employee.position;
+    this.post = employee.post;
 };
 
 Employee.prototype.save = function (callback) {
     //要存入数据库的用户文档
     var employee = {
         owner:this.owner,
-    	account: this.account,
+    	username: this.username,
         password: this.password,
         name: this.name,
         age: this.age,
         phone:this.phone,
-        position: this.position
+        post: this.post
     };
     //打开数据库
     mongodb.open(function (err, db) {
         if (err) {
             return callback(err);//错误，返回 err 信息
         }
+        //读取  employees 集合
         db.collection(employee.owner + '_employees', function (err, collection) {
             if (err) {
                 mongodb.close();
@@ -42,7 +44,7 @@ Employee.prototype.save = function (callback) {
 };
 
 //读取员工信息
-Employee.get = function (account, callback) {
+Employee.get = function (username, callback) {
     mongodb.open(function (err, db) {
         if (err) {
             return callback(err);//错误，返回 err 信息
@@ -54,14 +56,14 @@ Employee.get = function (account, callback) {
                 return callback(err);//错误，返回 err 信息
             }
             //查找账户（值为 account 一个文档
-            collection.findOne({account: account}, function (err, employee) {
+            collection.findOne({username: username}, function (err, employee) {
                 mongodb.close();
                 return err ? callback(err) : callback(null, employee);
             });
         });
     });
 };
-Employee.update = function (account, up, callback) {
+Employee.update = function (username, up, callback) {
     mongodb.open(function (err, db) {
         if (err) {
             return callback(err);//错误，返回 err 信息
@@ -71,7 +73,7 @@ Employee.update = function (account, up, callback) {
                 mongodb.close();
                 return callback(err);//错误，返回 err 信息
             }
-            collection.update({'account': account}, up, function (err) {
+            collection.update({'username': username}, up, function (err) {
                 mongodb.close();
             });
         });
