6c95a6ff5133a0407cfa16feda07ec176741dbb4
huixuli <1967250954@qq.com>
2018-05-29 00:23:29 +0800

    Update ingredient.js

--- models/ingredient.js
+++ models/ingredient.js
@@ -8,73 +8,68 @@ function Ingredient(ingredient) {
     this.stock = ingredient.stock; 
 };
 
-//存储用户信息
-Ingredient.prototype.save = function (callback) {
-    //要存入数据库的用户文档
-    var ingredient = {
-        owner:this.owner,
-        name: this.name,
-        price: this.price,
-        cost:this.cost,
-        stock: this.stock
-    };
-    //打开数据库
-    mongodb.open(function (err, db) {
+Ingredient.operation = function(owner, up, callback) {
+     mongodb.open(function (err, db) {
         if (err) {
+            mongodb.close();
             return callback(err);//错误，返回 err 信息
         }
-        //读取 users 集合
-        db.collection(ingredient.owner + '_ingredients', function (err, collection) {
+        db.collection(owner + '_ingredients', function (err, collection) {
             if (err) {
                 mongodb.close();
                 return callback(err);//错误，返回 err 信息
             }
-            //将用户数据插入 users 集合
-            collection.insert(ingredient, {safe: true}, function (err, ingredient) {
-                mongodb.close();
-                //成功！err 为 null，并返回存储后的用户文档
-                return err ? callback(err) : callback(null, ingredient.ops[0]);
-            });
-        });
-    });
-};
-
-//读取用户信息
-Ingredient.get = function (name, callback) {
-    mongodb.open(function (err, db) {
-        if (err) {
-            return callback(err);//错误，返回 err 信息
-        }
-        //读取 users 集合
-        db.collection(ingredient.owner + '_ingredients', function (err, collection) {
-            if (err) {
-                mongodb.close();
-                return callback(err);//错误，返回 err 信息
-            }
-            //查找用户名（name键）值为 name 一个文档
-            collection.findOne({name: name}, function (err, user) {
-                mongodb.close();
-                return err ? callback(err) : callback(null, ingredient);
-            });
-        });
-    });
-};
-
-Ingredient.update = function (name, up, callback) {
-    mongodb.open(function (err, db) {
-        if (err) {
-            return callback(err);//错误，返回 err 信息
-        }
-        db.collection(ingredient.owner + '_ingredients', function (err, collection) {
-            if (err) {
-                mongodb.close();
-                return callback(err);//错误，返回 err 信息
-            }
-            collection.update({'name': name}, up, function (err) {
-                mongodb.close();
-            });
+            var arr = Object.keys(up);
+            var len = arr.length / 5;
+           for (var i = 0; i < len; i++) {
+              var name = up['data[' + i + '][name]'];
+              var price = up['data[' + i + '][price]'];
+              var cost = up['data[' + i + '][cost]'];
+              var stock = up['data[' + i + '][stock]'];
+              if (up['data[' + i + '][op]'] == 'save') {
+                collection.findOne({ name: name }, function (err, ingredient) {
+                    if (ingredient) {
+                        var up1 = {
+                            $set: {
+                                'price': price,
+                                'cost': cost,
+                                'stock': stock
+                            }
+                        };
+                        collection.update({ 'name': name }, up1, function (err) {
+                                if (err) {
+                                    return callback(err);//错误，返回 err 信息
+                                }
+                            });
+                        }
+                        else {
+                            var newIngredient = new Ingredient({
+                                owner: owner,
+                                name: name,
+                                price: price,
+                                cost: cost,
+                                stock: stock
+                            });
+                            collection.insert(newIngredient, { safe: true }, function (err) {
+                                if (err) {
+                                    return callback(err);//错误，返回 err 信息
+                                }
+                            });
+                        }
+                    });
+                }
+                else if (up['data[' + i + '][op]'] == 'delete') {
+                    collection.remove({ 'name': name }, function (err) { });
+               }
+               if (i == len - 1) {
+                   setTimeout(function () {
+                       mongodb.close();
+                   }, 500); 
+               }
+            }  
         });
     });
 };
 
 module.exports = Ingredient;
+
