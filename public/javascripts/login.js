2ccea981def724257ac50b8edaeef3c6151658ec
Dennis <937889621@qq.com>
2018-05-31 00:42:02 +0800

    add employee login function

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,46 +1,82 @@
 (function() {
   var public = {
-    waitTime: 5
+    waitTime: 2
   }
 
   $(function() {
     $(".form-body input").blur(checkValidity)
     .blur();
     $(".form-header button").click(clearInfo);
-    $("[name='login']").submit(submitLoginForm);
+    $("a[name='employeeLogin']").click(showEmployeeLogin);
+    $("[name='managerLogin']").submit(submitManagerLoginForm);
+    $("[name='employeeLogin']").submit(submitEmployeeLoginForm);
     $("[name='regist']").submit(submitRegistForm);
   });
 
-  function submitLoginForm() {
-    if(!isAllCorrect("login")) {    //不合要求则阻止提交
-      $("#login .form-result").text("存在非法格式！");
+  function showEmployeeLogin() {
+    $("#managerLogin").modal("hide");
+    $("#employeeLogin").modal("show");
+  }
+
+  function submitManagerLoginForm() {
+    if(!isAllCorrect("managerLogin")) {    //不合要求则阻止提交
+      $("#managerLogin .form-result").text("存在非法格式！");
       return false;
     }
     
     $.ajax({    //采用ajax提交
       type: "POST",
       async: true,
-      data: $("[name='login']").serialize(),
+      data: $("[name='managerLogin']").serialize(),
       cache: false,
-      url: "?op=login",
+      url: "?op=managerLogin",
       success: function(result) {
-        $("#login .form-result").text("");
+        $("#managerLogin .form-result").text("");
         switch(result) {
-          case "success": success("登录");
+          case "success": success("店主登录");
             break;
-          case "notFound": $("#login .form-result").text("该用户不存在！");
+          case "notFound": $("#managerLogin .form-result").text("该用户不存在！");
             showValidity(false, "username");
             break;
-          case "wrongPassword": $("#login .form-result").text("密码错误！");
+          case "wrongPassword": $("#managerLogin .form-result").text("密码错误！");
             showValidity(false, "password");
             break;
           default: window.location.href = "/";
         }
       }
     });
-    
     return false; //阻止submit提交
-  } 
+  }
+
+  function submitEmployeeLoginForm() {
+    if(!isAllCorrect("employeeLogin")) {    //不合要求则阻止提交
+      $("#employeeLogin .form-result").text("存在非法格式！");
+      return false;
+    }
+    
+    $.ajax({    //采用ajax提交
+      type: "POST",
+      async: true,
+      data: $("[name='employeeLogin']").serialize(),
+      cache: false,
+      url: "?op=employeeLogin",
+      success: function(result) {
+        $("#employeeLogin .form-result").text("");
+        switch(result) {
+          case "success": success("员工登录");
+            break;
+          case "notFound": $("#employeeLogin .form-result").text("该用户不存在！");
+            showValidity(false, "username");
+            break;
+          case "wrongPassword": $("#employeeLogin .form-result").text("密码错误！");
+            showValidity(false, "password");
+            break;
+          default: window.location.href = "/";
+        }
+      }
+    });
+    return false; //阻止submit提交
+  }
 
   function submitRegistForm() {
     if(!isAllCorrect("regist")) {    //不合要求则阻止提交
@@ -80,17 +116,23 @@
   }
 
   function countDownToJumpPage(operation) {    //倒计时跳转页面
-      if(public.waitTime-- <= 0) {
+      if (public.waitTime-- <= 0) {
           clearTimeout(public.clock);
           //跳转到主页
-          window.location.href = 'user/?username=' + ((operation == "登录") ? $("#username").val() : $("#upUsername").val()) + '&info=personal';
+          if(operation == "店主登录" || operation == "注册") {
+            window.location.href = 'user/?username=' + ((operation == "登录") ? $("#mUsername").val() : $("#upUsername").val()) + '&info=personal';
+          } else {
+            window.location.href = 'employee/?username=' + $("#eUsername").val();
+          }
           return;
       }
       var result;
-      if(operation == "登录") {
-          result = $("#login .form-result");
+      if (operation == "店主登录") {
+        result = $("#managerLogin .form-result");
+      } else if (operation == "员工登录") {
+        result = $("#employeeLogin .form-result");
       } else {
-          result = $("#regist .form-result");
+        result = $("#regist .form-result");
       }
       result.text(operation + "成功！" + public.waitTime + "秒后跳转页面");
       public.clock = setTimeout(countDownToJumpPage.bind(null, operation), 1000);
