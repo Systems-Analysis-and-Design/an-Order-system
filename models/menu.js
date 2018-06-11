f27a11becf3a7d68f1ff3b7430008d8468eb8efb
huixuli <1967250954@qq.com>
2018-06-11 12:41:22 +0800

    Update menu.js

--- models/menu.js
+++ models/menu.js
@@ -23,63 +23,63 @@ Menu.gg = function (name, up, callback) {
             }
             var arr = Object.keys(up);
             var len = arr.length / 7;
-           for (var i = 0; i < len; i++) {
-                var menuname1 = up['data[' + i + '][name]'];
-                var imgSrc1 = up['data[' + i + '][imgSrc]'];
-                var ingredients1 = up['data[' + i + '][ingredients]'];
-                var cost1 = up['data[' + i + '][cost]'];
-                var price1 = up['data[' + i + '][price]'];
-                var class1 = up['data[' + i + '][class]'];
+            for (var i = 0; i < len; i++) {
+                (function(i){
+                    var menuname1 = up['data[' + i + '][name]'];
+                    var imgSrc1 = up['data[' + i + '][imgSrc]'];
+                    var ingredients1 = up['data[' + i + '][ingredients]'];
+                    var cost1 = up['data[' + i + '][cost]'];
+                    var price1 = up['data[' + i + '][price]'];
+                    var class1 = up['data[' + i + '][class]'];
+                    if (up['data[' + i + '][op]'] == 'save') {
 
-                if (up['data[' + i + '][op]'] == 'save') {
-
-                    collection.findOne({ name: menuname1 }, function (err, menu) {
-                        if (menu) {
-                            var up1 = {
-                                $set: {
-                                    'ingredients': ingredients1,
-                                    'imgSrc' :imgSrc1,
-                                    'cost': cost1,
-                                    'price': price1,
-                                    'class':class1
-                                }
-                            };
-                            collection.update({ 'name': menuname1 }, up1, function (err) {
-                                if (err) {
-                                    return callback(err);//错误，返回 err 信息
-                                }
-                            });
-                        }
-                        else {
-                            var newMenu = new Menu({
-                                owner: name,
-                                name: menuname1,
-                                class:class1,
-                                imgSrc:imgSrc1,
-                                ingredients: ingredients1,
-                                cost: cost1,
-                                price: price1
-                            });
-                            collection.insert(newMenu, { safe: true }, function (err) {
-                                console.log(newMenu);
-                                if (err) {
-                                    console.log(err);
-                                    return callback(err);//错误，返回 err 信息
-                                }
-                              //  console.log('2');
-                            });
-                        }
-                    });
-                }
-                else if (up['data[' + i + '][op]'] == 'delete') {
-                    collection.remove({ 'name': menuname1 }, function (err) { });
-               }
-               if (i == len - 1) {
-                   setTimeout(function () {
-                       mongodb.close();
-                   }, 500);
-                   
-               }
+                        collection.findOne({ name: menuname1 }, function (err, menu) {
+                            if (menu) {
+                                var up1 = {
+                                    $set: {
+                                        'ingredients': ingredients1,
+                                        'imgSrc' :imgSrc1,
+                                        'cost': cost1,
+                                        'price': price1,
+                                        'class':class1
+                                    }
+                                };
+                                collection.update({ 'name': menuname1 }, up1, function (err) {
+                                    if (err) {
+                                        return callback(err);//错误，返回 err 信息
+                                    }
+                                });
+                            }
+                            else {
+                                var newMenu = new Menu({
+                                    owner: name,
+                                    name: menuname1,
+                                    class:class1,
+                                    imgSrc:imgSrc1,
+                                    ingredients: ingredients1,
+                                    cost: cost1,
+                                    price: price1
+                                });
+                                collection.insert(newMenu, { safe: true }, function (err) {
+                                    console.log(newMenu);
+                                    if (err) {
+                                        console.log(err);
+                                        return callback(err);//错误，返回 err 信息
+                                    }
+                                  //  console.log('2');
+                                });
+                            }
+                        });
+                    }
+                    else if (up['data[' + i + '][op]'] == 'delete') {
+                        collection.remove({ 'name': menuname1 }, function (err) { });
+                   }
+                   if (i == len - 1) {
+                       setTimeout(function () {
+                           mongodb.close();
+                       }, 500);
+                   }
+                })(i);
             }
             
         });
