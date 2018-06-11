89b861dfd183cfc8708fca3752cc2434b78ab76b
huixuli <1967250954@qq.com>
2018-06-11 12:52:04 +0800

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -25,38 +25,38 @@ Employee.gg = function (name, up, callback) {
             var arr = Object.keys(up);
             var len = arr.length / 7;
             for (var i = 0; i < len; i++) { 
-                console.log(up);
-
-                var img1 = up['data[' + i + '][imgSrc]'];
-                var username1 = up['data[' + i + '][username]'];
-                var name1 = up['data[' + i + '][name]'];
-                var age1 = up['data[' + i + '][age]'];
-                var phone1 = up['data[' + i + '][phone]'];
-                var post1 = up['data[' + i + '][post]'];
-                if (up['data[' + i + '][op]'] == 'save') {
-                    var up1 = {
-                        $set: {
-                            'imgSrc': img1,
-                            'post': post1,
-                            'name': name1,
-                            'age': age1,
-                            'phone':phone1
-                        }
-                    };
-                    collection.update({ 'username': username1 }, up1, function (err) {
-                        if (err) {
-                            return res.json(err);
-                        }
-                    });
-                }
-                else if (up['data[' + i + '][op]'] == 'delete') {
-                            collection.remove({ 'username': username1 }, function (err) {});
-                }
-                if (i == len - 1) {
-                    setTimeout(function () {
-                        mongodb.close();
-                    }, 500);
-                }
+                (function(i){
+                    var img1 = up['data[' + i + '][imgSrc]'];
+                    var username1 = up['data[' + i + '][username]'];
+                    var name1 = up['data[' + i + '][name]'];
+                    var age1 = up['data[' + i + '][age]'];
+                    var phone1 = up['data[' + i + '][phone]'];
+                    var post1 = up['data[' + i + '][post]'];
+                    if (up['data[' + i + '][op]'] == 'save') {
+                        var up1 = {
+                            $set: {
+                                'imgSrc': img1,
+                                'post': post1,
+                                'name': name1,
+                                'age': age1,
+                                'phone':phone1
+                            }
+                        };
+                        collection.update({ 'username': username1 }, up1, function (err) {
+                            if (err) {
+                                return res.json(err);
+                            }
+                        });
+                    }
+                    else if (up['data[' + i + '][op]'] == 'delete') {
+                                collection.remove({ 'username': username1 }, function (err) {});
+                    }
+                    if (i == len - 1) {
+                        setTimeout(function () {
+                            mongodb.close();
+                        }, 500);
+                    }
+                })(i);
             }
         });
     });
