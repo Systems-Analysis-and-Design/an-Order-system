347b9737a2c2db4fed2bfb81fd7962a5df8e715b
Dennis <937889621@qq.com>
2018-06-11 14:12:34 +0800

    add QR code

--- public/javascripts/info-personal.js
+++ public/javascripts/info-personal.js
@@ -4,7 +4,14 @@
     $(".form-body input").blur();
     $(".form-body input").change(enableSubmit);
     $("form").submit(saveChange);
+    $("[name='createQRcode']").click(getQRcode);
   });
+
+  function getQRcode() {
+    var siteUrl = "http://" + window.location.host + "/" + $(".head-contents a").eq(0).text() + "/client?tableID=" + $("[name='tableID']").val();
+    var QRCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + siteUrl;
+    $(".QRCodeBody img").attr('src', QRCodeUrl);
+  }
 
   function enableSubmit() {
     $(this).parents(".form-body").next().find("input").removeAttr("disabled");
