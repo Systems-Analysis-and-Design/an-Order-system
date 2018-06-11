455dcd438b5391f36c4d467c64b3b7ce5dfeba5f
huixuli <1967250954@qq.com>
2018-06-11 12:51:31 +0800

    Update ingredient.js

--- models/ingredient.js
+++ models/ingredient.js
@@ -22,50 +22,52 @@ Ingredient.operation = function(owner, up, callback) {
             var arr = Object.keys(up);
             var len = arr.length / 5;
            for (var i = 0; i < len; i++) {
-              var name = up['data[' + i + '][name]'];
-              var price = up['data[' + i + '][price]'];
-              var cost = up['data[' + i + '][cost]'];
-              var stock = up['data[' + i + '][stock]'];
-              if (up['data[' + i + '][op]'] == 'save') {
-                collection.findOne({ name: name }, function (err, ingredient) {
-                    if (ingredient) {
-                        var up1 = {
-                            $set: {
-                                'price': price,
-                                'cost': cost,
-                                'stock': stock
-                            }
-                        };
-                        collection.update({ 'name': name }, up1, function (err) {
-                                if (err) {
-                                    return callback(err);//错误，返回 err 信息
+                (function(i){
+                    var name = up['data[' + i + '][name]'];
+                    var price = up['data[' + i + '][price]'];
+                    var cost = up['data[' + i + '][cost]'];
+                    var stock = up['data[' + i + '][stock]'];
+                    if (up['data[' + i + '][op]'] == 'save') {
+                        collection.findOne({ name: name }, function (err, ingredient) {
+                            if (ingredient) {
+                                var up1 = {
+                                    $set: {
+                                        'price': price,
+                                        'cost': cost,
+                                        'stock': stock
+                                    }
+                                };
+                                collection.update({ 'name': name }, up1, function (err) {
+                                        if (err) {
+                                            return callback(err);//错误，返回 err 信息
+                                        }
+                                    });
+                                }
+                                else {
+                                    var newIngredient = new Ingredient({
+                                        owner: owner,
+                                        name: name,
+                                        price: price,
+                                        cost: cost,
+                                        stock: stock
+                                    });
+                                    collection.insert(newIngredient, { safe: true }, function (err) {
+                                        if (err) {
+                                            return callback(err);//错误，返回 err 信息
+                                        }
+                                    });
                                 }
                             });
                         }
-                        else {
-                            var newIngredient = new Ingredient({
-                                owner: owner,
-                                name: name,
-                                price: price,
-                                cost: cost,
-                                stock: stock
-                            });
-                            collection.insert(newIngredient, { safe: true }, function (err) {
-                                if (err) {
-                                    return callback(err);//错误，返回 err 信息
-                                }
-                            });
-                        }
-                    });
-                }
-                else if (up['data[' + i + '][op]'] == 'delete') {
-                    collection.remove({ 'name': name }, function (err) { });
-               }
-               if (i == len - 1) {
-                   setTimeout(function () {
-                       mongodb.close();
-                   }, 500); 
-               }
+                        else if (up['data[' + i + '][op]'] == 'delete') {
+                            collection.remove({ 'name': name }, function (err) { });
+                       }
+                       if (i == len - 1) {
+                           setTimeout(function () {
+                               mongodb.close();
+                           }, 500); 
+                       }
+               })(i);
             }  
         });
     });
