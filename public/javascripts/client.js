14338141d2543e2f1a857020788b6866625fce5e
Leeegal <liguoai2010@outlook.com>
2018-06-11 12:46:37 +0800

    注释

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,24 +7,24 @@
         $(".tab-content").on("click", ".decreaseNum", decreaseNum);
         $(".img-responsive").each(fixSrc);
     });
-
+    // 动态生成菜品加入html模块
     function fixClassify() {
         var itemClass = $(this).find(".code-class").text();
         console.log($(this).html());
         $("#" + itemClass).append($(this).html());
     }
-
+    // 点击 + 操作
     function addNum() {
         var oldNum = parseInt($(this).prev().find("label").text());
         $(this).prev().find("label").text(oldNum + 1);
     }
-
+    // 点击 - 操作
     function decreaseNum() {
         var oldNum = parseInt($(this).next().find("label").text());
         if (oldNum <= 0) return;
         $(this).next().find("label").text(oldNum - 1);
     }
-
+    // 固定
     function fixSrc() {
         var src = $(this).attr("src");
         console.log(src);
--- public/javascripts/order_finish.js
+++ public/javascripts/order_finish.js
@@ -1,41 +1,41 @@
 (function() {
-	$(function() {
-		setOrderNum();
-		$("#post").click(submitForm);
-	});
+    $(function() {
+        setOrderNum();
+        $("#post").click(submitForm);
+    });
 
-	function getSearchString(key) {
-	    // 获取URL中?之后的字符
-	    var str = location.search;
-	    str = str.substring(1,str.length);
-	    
-	    // 以&分隔字符串，获得类似name=xiaoli这样的元素数组
-	    var arr = str.split("&");
-	    var obj = new Object();
-	    
-	    // 将每一个数组元素以=分隔并赋给obj对象    
-	    for(var i = 0; i < arr.length; i++) {
-	        var tmp_arr = arr[i].split("=");
-	        obj[decodeURIComponent(tmp_arr[0])] = decodeURIComponent(tmp_arr[1]);
-	    }
-	    return obj[key];
-  	}
+    function getSearchString(key) {
+        // 获取URL中?之后的字符
+        var str = location.search;
+        str = str.substring(1, str.length);
 
-	function setOrderNum() {
-		$("#orderNum").val(getSearchString("id"));
-	}
+        // 以&分隔字符串，获得类似name=xiaoli这样的元素数组
+        var arr = str.split("&");
+        var obj = new Object();
 
-	function submitForm() {
-		$.ajax({    //采用ajax提交
-	      type: "POST",
-	      async: true,
-	      data: $("[name='evaluation']").serialize(),
-	      dataType: "json",
-	      url: "",
-	      success: function(result) {
-	      		alert("评价提交成功！");
-	      }
-	    });
-	    return false; //阻止submit提交
-	}
+        // 将每一个数组元素以=分隔并赋给obj对象    
+        for (var i = 0; i < arr.length; i++) {
+            var tmp_arr = arr[i].split("=");
+            obj[decodeURIComponent(tmp_arr[0])] = decodeURIComponent(tmp_arr[1]);
+        }
+        return obj[key];
+    }
+
+    function setOrderNum() {
+        $("#orderNum").val(getSearchString("id"));
+    }
+
+    function submitForm() {
+        $.ajax({ //采用ajax提交
+            type: "POST",
+            async: true,
+            data: $("[name='evaluation']").serialize(),
+            dataType: "json",
+            url: "",
+            success: function(result) {
+                alert("评价提交成功！");
+            }
+        });
+        return false; //阻止submit提交
+    }
 })();
