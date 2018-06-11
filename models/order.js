d321aef1e2b621c4e9a2999c00e4c26fd4d18bee
Jia Li <1335275309@qq.com>
2018-06-11 19:27:15 +0800

    Update order.js

--- models/order.js
+++ models/order.js
@@ -16,56 +16,17 @@ function Order(order) {
     this.state = order.state;
 };
 
-// Order.prototype.save = function (callback) {
-//     //要存入数据库的用户文档
-//     var order = {
-//         id: this.id,
-//         streamid:this.streamid,
-//         owner: this.owner,
-//         menu_name:this.menu_name,
-//         taste:this.taste,
-//         speedOfProduction:this.speedOfProduction,
-//         serviceAttitude:this.serviceAttitude,
-//         totalEvaluation: this.totalEvaluation,
-//         cost: this.cost,
-//         price: this.price,
-//         state: this.state
-//     };
-//     //打开数据库
-//     mongodb.open(function (err, db) {
-//         if (err) {
-//             return callback(err);//错误，返回 err 信息
-//         }
-//         //读取 users 集合
-//         db.collection(owner+'_orders', function (err, collection) {
-//             if (err) {
-//                 mongodb.close();
-//                 return callback(err);//错误，返回 err 信息
-//             }
-//             //将用户数据插入 users 集合
-//             collection.insert(order, {safe: true}, function (err, order) {
-//                        mongodb.close();
-                
-//                 //成功！err 为 null，并返回存储后的用户文档
-//                 return err ? callback(err) : callback(null, order.ops[0]);
-//             });
-//         });
-//     });
-// };
-
-//读取用户信息
 Order.get = function (owner, streamid, callback) {
     mongodb.open(function (err, db) {
         if (err) {
             return callback(err);//错误，返回 err 信息
         }
-        //读取 users 集合
+
         db.collection(owner+'_orders', function (err, collection) {
             if (err) {
                 mongodb.close();
                 return callback(err);//错误，返回 err 信息
             }
-            //查找用户名（name键）值为 name 一个文档
             collection.findOne({streamid: streamid}, function (err, order) {
                 mongodb.close();
                 return err ? callback(err) : callback(null,order);
@@ -75,23 +36,18 @@ Order.get = function (owner, streamid, callback) {
 };
 
 Order.update = function (owner, streamid, up, callback) {
-    console.log("222");
     mongodb.open(function (err, db) {
         if (err) {
             return callback(err);//错误，返回 err 信息
         }
-        console.log("333");
         db.collection('qqqqqq_orders', function (err, collection) {
             if (err) {
                 mongodb.close();
                 return callback(err);//错误，返回 err 信息
             }
-            console.log("444");
             collection.update({streamid: streamid}, up, function (err) {
                 if(err) {
-                    console.log("666");
                 }
-                console.log("555");
                 mongodb.close();
             });
         });
